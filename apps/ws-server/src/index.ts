ef94a1af0c3edc05a50559ff400b877e0b2a2376
n3rddev <n3rddev.code@gmail.com>
2025-09-04 22:05:52 +0530

    MonoRepo initialisation & project structure

---
+++ apps/ws-server/src/index.ts
@@ -0,0 +1,11 @@
+import ws from 'ws';
+
+const wss = new ws.WebSocketServer({port:8080});
+
+wss.on('connection', (ws)=>{
+    
+    ws.on('message',(data)=>{
+        ws.send('pong');
+    })
+
+})
