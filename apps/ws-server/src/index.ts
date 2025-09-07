53774c39269e7222433e80e480210242ccc73305
n3rddev <n3rddev.code@gmail.com>
2025-09-07 13:23:59 +0530

    ws layer started

--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -1,9 +1,20 @@
 import ws from 'ws';
 import jwt from 'jsonwebtoken';
-const secret = 'maiNahiBataunga';
 import { JwtPayload } from 'jsonwebtoken';
+import {jwt_secret} from '@repo/backend-common/config'
 
 const wss = new ws.WebSocketServer({port:8080});
+
+function checkuser (token: string): string | null{
+    const decoded = jwt.verify(token , jwt_secret);
+    if(typeof decoded == "string"){
+        return null;
+    }
+    if(!(decoded as JwtPayload).userId || !decoded){
+        return null;
+    }
+    return decoded.userId;
+}
 
 wss.on('connection', (ws, request)=>{
     
@@ -13,12 +24,13 @@ wss.on('connection', (ws, request)=>{
     }
     const queryparams = new URLSearchParams(url.split('?')[1]);
     const token = queryparams.get('token') || "";
-    const decoded = jwt.verify(token, secret);
+    const userAuthenticated = checkuser(token);
 
-    if(!(decoded as JwtPayload).userId || !decoded){
-        ws.close();
-        return;
+    if(!userAuthenticated){
+        ws.close()
     }
+    
+    
 
     ws.on('message',(data)=>{
         ws.send('pong');
